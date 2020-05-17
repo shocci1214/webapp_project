66906bd6660076b3ff15c71c2636fb04c8538551
shocci1214 <shocci1214@gmail.com>
2020-05-17 23:43:44 +0900

    DAOを使って、一覧を表示できるようにした

--- src/servlet/EmployeeListServlet.java
+++ src/servlet/EmployeeListServlet.java
@@ -5,6 +5,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,6 +14,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.EmployeeDAO;
+import model.entity.EmployeeBean;
 
 /**
  * 従業員一覧表示を行うサーブレット
@@ -34,6 +39,23 @@ public class EmployeeListServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        // リクエストオブジェクトのエンコーディング方式の指定
+        request.setCharacterEncoding("UTF-8");
+
+        // DAOの生成
+        EmployeeDAO dao = new EmployeeDAO();
+
+        try {
+            // DAOの利用
+            List<EmployeeBean> employeeList = dao.selectAll();
+
+            // リクエストスコープへの属性の設定
+            request.setAttribute("employeeList", employeeList);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
         // リクエストの転送
         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/employee-list.jsp");
         rd.forward(request, response);
@@ -45,7 +67,6 @@ public class EmployeeListServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
 
     }
 
